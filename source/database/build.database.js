f33c1d8c4c456c37177a900f239f9dea3479202d
Asad Razvi <debugme@hotmail.com>
2017-04-16 22:46:47 +0100

    tidy up tests

--- source/database/build.database.js
+++ source/database/build.database.js
@@ -4,26 +4,6 @@ const buildDatabase = (options) => {
   mongoose.Promise = global.Promise
   const databaseUri = options.mongodbUri
   mongoose.connect(databaseUri)
-  console.log(`database: ${databaseUri}`)
-
-  mongoose.connection.on('connected', () => {
-    console.log('database connected')
-  })
-
-  mongoose.connection.on('error', () => {
-    console.log('database connection error')
-  })
-
-  mongoose.connection.on('disconnected', () => {
-    console.log('database disconnected')
-  })
-
-  process.on('SIGINT', () => {
-    console.log('database disconnected due to application termination request')
-    mongoose.connection.close()
-    process.exit(0)
-  })
-
   return mongoose
 }
 
--- source/server/server.js
+++ source/server/server.js
@@ -7,5 +7,25 @@ import buildServer from './build.server'
 const env = path.resolve('.env')
 dotenv.config({ path: env })
 
-buildDatabase(process.env)
-buildServer(process.env)
+const database = buildDatabase(process.env)
+const server = buildServer(process.env)
+
+database.connection.on('connected', () => {
+  console.log('database connected')
+})
+
+database.connection.on('error', () => {
+  console.log('database connection error')
+})
+
+database.connection.on('disconnected', () => {
+  console.log('database disconnected')
+})
+
+process.on('SIGINT', () => {
+  console.log('database disconnected due to application termination request')
+  database.connection.close()
+  console.log('server disconnected due to application termination request')
+  server.close()
+  process.exit(0)
+})
